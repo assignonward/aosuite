76988da4bbe82753e9f2daa37e058e91bf2337fb
Assign Onward <assign.onward@gmail.com>
2018-06-09 21:15:55 -0400

    convert OpenPGP to repo linked submodule

--- OpenPGP/tests/testcases/testvectors/msg.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PGP_TEST_MESSAGE__
-#define __PGP_TEST_MESSAGE__
-
-#include <string>
-
-static const std::string MESSAGE = "The magic words are squeamish ossifrage\n";
-
-#endif
--- OpenPGP/tests/testcases/testvectors/pass.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PGP_TEST_PASSPHRASE__
-#define __PGP_TEST_PASSPHRASE__
-
-#include <string>
-
-static const std::string PASSPHRASE = "test";
-
-#endif
