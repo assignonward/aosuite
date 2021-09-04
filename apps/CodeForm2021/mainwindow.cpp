de0dcb93d6d163d69e6a70dce0b29be1d13d6b05
Assign Onward <assign.onward@gmail.com>
2021-09-04 17:25:05 -0400

    CodeForm: doing some rules checking.

--- apps/CodeForm2021/mainwindow.cpp
+++ apps/CodeForm2021/mainwindow.cpp
@@ -14,10 +14,72 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::on_update_clicked()
+{ init();
+  firstPass();
+  translateNotes();
+  t.append( "\n" );
+  translateRicey();
+  showResults();
+}
+
+/**
+ * @brief MainWindow::init - reset everything ready for a new translation
+ */
+void MainWindow::init()
+{ v.clear();
+  t.clear();
+  notesNumChar.clear();
+  notesList = ui->notes->toPlainText().split("\n",QString::SkipEmptyParts);
+  riceyList = ui->ricey->toPlainText().split("\n",QString::SkipEmptyParts);
+  maxNameLength = 0;
+  maxNumLength  = 0;
+}
+
+/**
+ * @brief MainWindow::showResults - update the translations window
+ */
+void MainWindow::showResults()
 { ui->translations->clear();
-  v.clear(); t.clear();
-  QStringList rList = ui->ricey->toPlainText().split("\n",QString::SkipEmptyParts);
-  foreach ( QString line, rList )
+  if ( v.size() > 0 )
+    t.append( "\n"+v );
+  ui->translations->append( t );
+}
+
+/**
+ * @brief MainWindow::firstPass - pick up some easy stuff for use in the later passes
+ */
+void MainWindow::firstPass()
+{ foreach ( QString line, riceyList )
+    { QStringList words = line.split(" ",QString::SkipEmptyParts);
+      if ( words.size() > 1 )
+        { if ( words.at(0).size() > maxNameLength )
+            maxNameLength = words.at(0).size();
+          if ( words.at(1).size() > maxNumLength )
+            maxNumLength = words.at(1).size();
+        }
+       else
+        { v.append( QString( "WARN: short line '%1' in Ricey Code Definitions.\n" ).arg( line ) );
+        }
+    }
+  foreach ( QString line, notesList )
+    { QStringList words = line.split(" ",QString::SkipEmptyParts);
+      if ( words.size() > 3 )
+        { if ( words.at(1).startsWith( "0x" ) )
+            { if ( words.at(1).size() == 3 )
+                { if ( words.at(3).size() == 1 )
+                    { notesNumChar.insert( words.at(1).at(2), words.at(3).at(0) );
+                    } else { v.append( QString( "WARN: 4th word in Code Notes '%1' isn't length 1.\n" ).arg( line ) ); }
+                } else { v.append( QString( "WARN: 2nd word in Code Notes '%1' isn't length 3.\n" ).arg( line ) ); }
+            } else { v.append( QString( "WARN: 2nd word in Code Notes '%1' doesn't start with 0x.\n" ).arg( line ) ); }
+        } else { v.append( QString( "WARN: line '%1' in Code Notes doesn't have 4 or more words.\n" ).arg( line ) ); }
+    }
+}
+
+/**
+ * @brief MainWindow::translateRicey - also check for correctness as we go
+ */
+void MainWindow::translateRicey()
+{ foreach ( QString line, riceyList )
     { qint32 ics = line.indexOf( "//" );
       if ( ics < 0 )
         v.append( "WARN: missing // in: "+line+"\n" );
@@ -31,18 +93,19 @@ void MainWindow::on_update_clicked()
                 v.append( QString( "WARN: rules check failed in: %1\n" ).arg( line ) );
                else
                 { // Temporary output for warm fuzzies
-                  t.append( QString( "#define KTC_%1 %2 %3\n" ).arg( lList.at(0) ).arg( lList.at(1) ).arg( line.mid(ics) ) );
+                  QString name = lList.at(0);
+                  while ( name.size() < maxNameLength )
+                    name.append( QChar(' ') );
+                  QString num = lList.at(1);
+                  while ( num.size() < maxNumLength )
+                    num.prepend( QChar(' ') );
+                  t.append( QString( "#define KTC_%1 %2 %3\n" ).arg( name ).arg( num ).arg( line.mid(ics) ) );
                 }
-
             }
         }
     }
-
-  t.append( ui->notes->toPlainText() );
-
-  t.append( v );
-  ui->translations->append( t );
 }
+
 
 /**
  * @brief MainWindow::rulesCheck - operating on the ricey code definitions lines
@@ -52,17 +115,36 @@ void MainWindow::on_update_clicked()
  */
 bool MainWindow::rulesCheck( QString name, QString num )
 { if ( name.size() < 2 )
-    { v.append( QString( "rulesCheck: name '%1' too short" ).arg( name ) );
+    { v.append( QString( "rulesCheck: name '%1' too short.\n" ).arg( name ) );
       return false;
     }
   if ( num.size() < 4 )
-    { v.append( QString( "rulesCheck: num '%1' too short" ).arg( num ) );
+    { v.append( QString( "rulesCheck: num '%1' too short.\n" ).arg( num ) );
       return false;
     }
   if ( !num.startsWith( "0x" ) )
-    { v.append( QString( "rulesCheck: num '%1' does not start with 0x" ).arg( num ) );
+    { v.append( QString( "rulesCheck: num '%1' does not start with 0x.\n" ).arg( num ) );
+      return false;
+    }
+  QChar numlc = num.at( num.size() - 1 );
+  if ( !notesNumChar.keys().contains( numlc ) )
+    { v.append( QString( "rulesCheck: num '%1' does not end with a recognized code.\n" ).arg( num ) );
+      return false;
+    }
+  QChar namelc = name.at( name.size() - 1 );
+  if ( notesNumChar[numlc] != namelc )
+    { v.append( QString( "rulesCheck: name '%1' does not end with the expected character %2.\n" ).arg( name ).arg( notesNumChar[numlc] ) );
       return false;
     }
   // More to come
   return true;
+}
+
+/**
+ * @brief MainWindow::translateNotes - write the .h code defines for the notes (Ricey Data Types)
+ */
+void MainWindow::translateNotes()
+{ foreach ( QString line, notesList )
+    { t.append( QString( "#define RDT_%1\n" ).arg(line) );
+    }
 }
--- apps/CodeForm2021/mainwindow.h
+++ apps/CodeForm2021/mainwindow.h
@@ -14,14 +14,23 @@ class MainWindow : public QMainWindow
 public:
         MainWindow(QWidget *parent = nullptr);
        ~MainWindow();
+  void  init();
+  void  firstPass();
+  void  showResults();
+  void  translateRicey();
+  void  translateNotes();
   bool  rulesCheck( QString, QString );
 
 public slots:
   void  on_update_clicked();
 
 public:
-    QString t; // translation
-    QString v; // violations
+            QString  t; // translation
+            QString  v; // violations
+             qint32  maxNameLength;
+             qint32  maxNumLength;
+        QStringList  riceyList,notesList;
+ QHash<QChar, QChar> notesNumChar;
 
 private:
     Ui::MainWindow *ui;
